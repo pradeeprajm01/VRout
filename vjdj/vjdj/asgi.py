9cee223792951036ae02ba905e42e7da8f775f45
Vijay-0404 <vijaymkvpy0105@gmail.com>
2022-02-24 11:50:56 +0530

    newlycreated

--- vjdj/vjdj/asgi.py
+++ vjdj/vjdj/asgi.py
@@ -1,16 +1,16 @@
 """
-ASGI config for VTsite project.
+ASGI config for vjdj project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'VTsite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vjdj.settings')
 
 application = get_asgi_application()
