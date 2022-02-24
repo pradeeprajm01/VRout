8b75470f9438b5f7738a17b518d51f9581e2f2db
Pradeep Raj <mpradeepraj004mpr@gmail.com>
2022-02-24 11:19:03 +0530

    VTsite Created

--- VTsite/homepage/apps.py
+++ VTsite/homepage/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class Vjapp1Config(AppConfig):
+class HomepageConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'vjapp1'
+    name = 'homepage'
