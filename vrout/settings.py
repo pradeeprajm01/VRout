9303974247c93ed4a0186b06e150ee664abf5384
Vijay-0404 <vijaymkvpy0105@gmail.com>
2022-03-11 20:53:00 +0530

    checked

--- vrout/settings.py
+++ vrout/settings.py
@@ -105,9 +105,11 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')  
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#MEDIA_URL = '/media/'
+STATICFILES_DIRS =[
+    os.path.join(BASE_DIR, 'static')
+]  
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
